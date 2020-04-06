66319a7e38ef9eacea006aacfa3ae79021e8107b
mbeni12333 <mbeni12333@gmail.com>
2020-04-06 17:31:44 +0200

    clean up

--- blocks.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <vector>
-
-enum class BlockType{ StartBlock = 0, EndBlock, ObstacleBlock, NeutralBlock, OpenBlock, ClosedBlock, PathBlock };
-using Board = std::vector<std::vector<BlockType>>;
